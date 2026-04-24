06d639cad46a0d897919990e5976fb54ce61b410
fazriravael <zrii0610@gmail.com>
2026-04-24 09:22:30 +0700

    Upload Github Pertamaku

---
+++ data.js
@@ -0,0 +1,11 @@
+const databaseProyek = {
+    "sistem_pos": {
+        judul: "sistem pos untuk PT ABC",
+        keahlian: ["html"],
+        gambar: "https://upload.wikimedia.org/wikipedia/commons/1/18/ABC_food_%282016%29.svg",
+        latar: "ini adalah projek magang saya untuk PT ABC",
+        tantangan: "kebutuhan user dalam PT ABC sangat beragam",
+        pelajaran: "saya mendapat pelajaran sistem POS pada PT ABC sangat ideal untuk skala kecil",
+        link: ""
+    },
+};
